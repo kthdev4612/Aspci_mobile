3c86938c8f3569137ff3a50b09c526320c088e6f
#kth-dev <tahajean-claude.kouizibo@tornixtech.com>
2023-12-08 18:28:56 +0000

    mise a jour

--- src/app/main/pointage/pointage.page.ts
+++ src/app/main/pointage/pointage.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BarcodeScanner,BarcodeFormat,LensFacing,Barcode} from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pointage',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PointagePage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
   }
 
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+
+  ngOnInit() {
+      BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
+  }
 }
